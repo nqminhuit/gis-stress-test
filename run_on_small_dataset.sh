3fc68a68011591ee8210e112a21efd0ba64a478a
minh <nqminhuit@gmail.com>
2024-07-21 13:56:34 +0700

    feat: Run test on specific gis commit
    fix: #4

--- run_on_small_dataset.sh
+++ run_on_small_dataset.sh
@@ -2,6 +2,7 @@
 
 source ./scripts/common.sh
 
+podman build --build-arg GIS_COMMIT=$1 -f scripts/Containerfile-jar-gis -t build-jar-gis .
 podman kube play --replace --build small_dataset.yaml
 
 printf "\nPlease wait while dataset is being setup...\n"
